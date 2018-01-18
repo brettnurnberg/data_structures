ee314ab8455af8e45ca2c653fdcb2562b8a4ff20
brettnurnberg <brettnurnberg@ksu.edu>
2018-01-18 15:52:42 -0600

    Added prepend, append, insert, and delete functions for singly linked list.

--- data_structures/data_structures/data_structures.cpp
+++ data_structures/data_structures/data_structures.cpp
@@ -16,23 +16,36 @@ typedef struct sll_node
 /* print all node data in a singly linked list */
 void sll_print(sll_node* head);
 
+/* prepend node to linked list */
+void sll_prepend(sll_node** head, int data);
+
+/* insert node to linked list */
+void sll_insert(sll_node** prev_node, int new_data);
+
+/* append a node to the end of a list */
+void sll_append(sll_node** head, int new_data);
+
+/* delete a node with the given data */
+void sll_delete_key(sll_node** head, int key);
 
 
+
+
+
+/*--------------------------------------
+Main
+--------------------------------------*/
 
 int main()
 {
-	sll_node* head = (sll_node*)malloc(sizeof(sll_node));
-	sll_node* node2 = (sll_node*)malloc(sizeof(sll_node));
-	sll_node* node3 = (sll_node*)malloc(sizeof(sll_node));
+	sll_node* head = NULL;
 
-	head->data = 0;
-	head->next = node2;
+	sll_append(&head, 1);
+	sll_append(&head, 2);
+	sll_append(&head, 3);
+	sll_append(&head, 4);
 
-	node2->data = 1;
-	node2->next = node3;
-
-	node3->data = 2;
-	node3->next = NULL;
+	sll_delete_key(&head, 1);
 
 	sll_print(head);
 
@@ -40,6 +53,9 @@ int main()
 
     return 0;
 }
+
+
+
 
 
 /*--------------------------------------
@@ -54,4 +70,107 @@ void sll_print(sll_node* n)
 		printf("%d\n", n->data);
 		n = n->next;
 	}
+}
+
+
+/* prepend a node to the top of a list */
+void sll_prepend(sll_node** head, int data)
+{
+	sll_node* new_node;
+
+	new_node = (sll_node*)malloc(sizeof(sll_node));
+
+	new_node->data = data;
+
+	new_node->next = (*head);
+
+	*head = new_node;
+
+}
+
+
+/* insert a node after a given node */
+void sll_insert(sll_node** prev_node, int new_data)
+{
+	sll_node* new_node;
+
+	new_node = (sll_node*)malloc(sizeof(sll_node));
+
+	if (*prev_node)
+	{
+		new_node->data = new_data;
+		new_node->next = (*prev_node)->next;
+		(*prev_node)->next = new_node;
+	}
+	else
+	{
+		(*prev_node) = new_node;
+		(*prev_node)->data = new_data;
+		(*prev_node)->next = NULL;
+	}
+	
+}
+
+
+/* append a node to the end of a list */
+void sll_append(sll_node** head, int new_data)
+{
+	sll_node* new_node;
+	sll_node* prev_node;
+
+	new_node = (sll_node*)malloc(sizeof(sll_node));
+
+	if (*head)
+	{
+		new_node->data = new_data;
+		new_node->next = NULL;
+
+		prev_node = *head;
+
+		while (prev_node->next)
+		{
+			prev_node = prev_node->next;
+		}
+
+		prev_node->next = new_node;
+	}
+	else
+	{
+		(*head) = new_node;
+		(*head)->data = new_data;
+		(*head)->next = NULL;
+	}
+
+}
+
+
+/* delete a node with the given data */
+void sll_delete_key(sll_node** head, int key)
+{
+	if (*head)
+	{
+		sll_node* prev_node;
+		sll_node* crnt_node;
+
+		prev_node = NULL;
+		crnt_node = (*head);
+
+		while (crnt_node && crnt_node->data != key)
+		{
+			prev_node = crnt_node;
+			crnt_node = crnt_node->next;
+		}
+
+		if (crnt_node && prev_node)
+		{
+			prev_node->next = crnt_node->next;
+			free(crnt_node);
+		}
+		else if (crnt_node)
+		{
+			(*head) = crnt_node->next;
+			free(crnt_node);
+		}
+	}
+
 }
